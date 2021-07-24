200102972ad0fd33f2b033130d781ab1ac1617b6
enrsa <69920761+enrsa@users.noreply.github.com>
2021-07-24 11:58:56 +0530

    Update 1. basic.php

--- plugins/1. basic.php
+++ plugins/1. basic.php
@@ -1,2 +1,36 @@
 <?php
+/**
+ * Plugin Name
+ *
+ * @package           PluginPackage
+ * @author            Your Name
+ * @copyright         2019 Your Name or Company Name
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Plugin Name
+ * Plugin URI:        https://example.com/plugin-name
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Your Name
+ * Author URI:        https://example.com
+ * Text Domain:       plugin-slug
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ * Domain Path:       /languages
+ */
 
+register_activation_hook( __FILE__, function(){
+    // Activation code
+} );
+
+register_deactivation_hook( __FILE__, function(){
+    // Deactivation code
+});
+
+register_uninstall_hook( __FILE__, function(){
+    // Uninstalled code
+});
